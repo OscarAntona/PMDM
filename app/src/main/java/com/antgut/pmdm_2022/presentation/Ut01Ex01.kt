f0ee8f84929d839796fad6e736916d6119e572d1
oscarinin11 <oscarinin11@gmail.com>
2022-09-27 16:52:00 +0200

    Primer Ejercicio para colocar textos en la pantalla en horizontal y vertical

---
+++ app/src/main/java/com/antgut/pmdm_2022/presentation/Ut01Ex01.kt
@@ -0,0 +1,12 @@
+package com.antgut.pmdm_2022.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.antgut.pmdm_2022.R
+
+class Ut01Ex01 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ut01ex01)
+    }
+}
