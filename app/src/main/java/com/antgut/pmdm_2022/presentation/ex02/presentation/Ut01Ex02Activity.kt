77e03b960ae50f7a84815ebabb44d2f22c96a94b
OscarAntona <53609303+OscarAntona@users.noreply.github.com>
2022-09-28 19:04:51 +0200

    PMDM new activity using textView, imageView and linearLayout

---
+++ app/src/main/java/com/antgut/pmdm_2022/presentation/ex02/presentation/Ut01Ex02Activity.kt
@@ -0,0 +1,11 @@
+package com.antgut.pmdm_2022.presentation.ex02.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Ut01Ex02Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ut01_ex02)
+    }
+}
