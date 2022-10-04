8417b153efd915673b15c37ca100e0cec5572788
OscarAntona <53609303+OscarAntona@users.noreply.github.com>
2022-10-04 16:25:22 +0200

    Commit ejercicio Ut01Ex02b, ficha hamburguesa

--- app/src/main/java/com/antgut/pmdm_2022/presentation/ex02b/presentation/Ut01Ex02b.kt
+++ app/src/main/java/com/antgut/pmdm_2022/presentation/ex02b/presentation/Ut01Ex02b.kt
@@ -1,12 +1,12 @@
-package com.antgut.pmdm_2022.presentation.ex02.presentation
+package com.antgut.pmdm_2022.presentation.ex02b.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.antgut.pmdm_2022.R
 
-class Ut01Ex02Activity : AppCompatActivity() {
+class Ut01Ex02b : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ut01_ex02)
+        setContentView(R.layout.activity_ut01_ex02b)
     }
 }
