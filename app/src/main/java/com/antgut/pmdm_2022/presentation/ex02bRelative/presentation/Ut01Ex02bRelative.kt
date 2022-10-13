a50b86ca61e420d5039f051aa70292eb1ad25b24
OscarAntona <53609303+OscarAntona@users.noreply.github.com>
2022-10-13 15:35:28 +0200

    UT01EX04/UT01EX05

--- app/src/main/java/com/antgut/pmdm_2022/presentation/ex02bRelative/presentation/Ut01Ex02bRelative.kt
+++ app/src/main/java/com/antgut/pmdm_2022/presentation/ex02bRelative/presentation/Ut01Ex02bRelative.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.antgut.pmdm_2022.R
 
-class Ut01Ex02b : AppCompatActivity() {
+class Ut01Ex02bRelative : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ut01_ex02brelative)
--- app/src/main/java/com/antgut/pmdm_2022/presentation/ex05/Ut01Ex05.kt
+++ app/src/main/java/com/antgut/pmdm_2022/presentation/ex05/Ut01Ex05.kt
@@ -1,12 +1,12 @@
-package com.antgut.pmdm_2022.presentation.ex02b.presentation
+package com.antgut.pmdm_2022.presentation.ex05
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.antgut.pmdm_2022.R
 
-class Ut01Ex02b : AppCompatActivity() {
+class Ut01Ex05 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ut01_ex02brelative)
+        setContentView(R.layout.activity_ut01_ex05)
     }
 }
