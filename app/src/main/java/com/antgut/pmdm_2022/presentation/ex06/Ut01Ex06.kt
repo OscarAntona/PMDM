a28eecab0e6ff555c3d636875f9052484451db75
OscarAntona <53609303+OscarAntona@users.noreply.github.com>
2022-10-24 18:49:52 +0200

    ejercicio 6 tapa con constraint layout

---
+++ app/src/main/java/com/antgut/pmdm_2022/presentation/ex06/Ut01Ex06.kt
@@ -0,0 +1,12 @@
+package com.antgut.pmdm_2022.presentation.ex06
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.antgut.pmdm_2022.R
+
+class Ut01Ex06 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ut01_ex05)
+    }
+}
